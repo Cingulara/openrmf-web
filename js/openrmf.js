6ee4f6c358b4351823d2dc09d08da91c20945005
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-05-26 10:58:59 -0400

    Fixing templates showing score

--- js/openrmf.js
+++ js/openrmf.js
@@ -17,57 +17,86 @@ async function getChecklistTotalCount() {
  * Template listing functions
  ************************************/
 async function getTemplates(latest) {
+	$.blockUI({ message: "Updating the checklist listing..." }); 
 	var url = templateAPI;	
 	let response = await fetch(url);
 	// parse the result regardless of the one called as the DIV are the same on Dashboard/index and the checklists pages
   if (response.ok) {
 			var data = await response.json()
-			var intNaF = 0;
-			var intNA = 0;
-			var intOpen = 0;
-			var intNR = 0;
-			// cycle through the data and fill in the data table at id tblChecklistListing div.html()
-			var table = "";
-			table += '<table class="table table-condensed table-hover table-bordered table-responsive-md"><thead><tr><th class="tabco1">Name</th>'
-			table += '<th class="tabco2">Not a Finding</th><th class="tabco3">Not Applicable</th><th class="tabco4">Open</th><th class="tabco5">Not Reviewed</th>'
-			table += '</tr></thead><tbody>'
-			if (data.length == 0) {$("#tblChecklistListing").html("There are currently no STIG checklist templates uploaded. Go to the Upload page to add your first one.");}
+
+			var table = $('#tblChecklistListing').DataTable(); // the datatable reference to do a row.add() to
+			table.clear();
+			var checklistLink = "";
+			if (data.length == 0) {
+				$.unblockUI();
+				var alertText = 'There are no STIG templates uploaded. Please go to the Upload page to add your first.';
+				alertText += '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
+				alertText += '<span aria-hidden="true">&times;</span></button>';
+				$("#divMessaging").html(alertText);
+				$("#divMessaging").show();
+			}
 			else {
-					for (const item of data) {
-					table += '<tr><td class="tabco1"><a href="single-template.html?id=' + item.internalId + '">'
-					table += item.title
-					intNaF = 0;
-					intNA = 0;
-					intOpen = 0;
-					intNR = 0;
-					// var score = await getScoreForTemplateListing(item.rawChecklist);
-					// if (score) {
-					// 	intNaF = score.totalNotAFinding;
-					// 	intNA = score.totalNotApplicable;
-					// 	intOpen = score.totalOpen;
-					// 	intNR = score.totalNotReviewed;
-					// }
-					table += '</a><br /><span class="small">last updated on '
+				$("#divMessaging").html('');
+				$("#divMessaging").hide();
+				for (const item of data) {
+					checklistLink = '<a href="single-template.html?id=' + item.internalId + '">'
+					checklistLink += item.title
+					checklistLink += '</a><br /><span class="small">last updated on '
 					if (item.updatedOn) {
-							table += moment(item.updatedOn).format('MM/DD/YYYY h:mm a');
+						checklistLink += moment(item.updatedOn).format('MM/DD/YYYY h:mm a');
 					}
 					else {
-						table += moment(item.created).format('MM/DD/YYYY h:mm a');
+						checklistLink += moment(item.created).format('MM/DD/YYYY h:mm a');
 					}
-					table += '</span></td><td class="tabco2"><i class="fa" aria-hidden="true"></i>' + intNaF.toString() + '</td>'
-					table += '<td class="tabco3"><i class="fa" aria-hidden="true"></i>' + intNA.toString() + '</td>'
-					table += '<td class="tabco4"><i class="fa" aria-hidden="true"></i>' + intOpen.toString() + '</td>'
-					table += '<td class="tabco5"><i class="fa" aria-hidden="true"></i>' + intNR.toString() + '</td>'
-					table += '</tr>'
+					// now get the score
+					var score = null;
+					var formData = new FormData();
+					formData.append("rawChecklist", item.rawChecklist);
+					$.ajax({
+						url : scoreAPI,
+						data : formData,
+						type : 'POST',
+						processData: false,
+						contentType: false,
+						success : function(data){
+							score = data;
+							if (score) {
+								// dynamically add to the datatable but only show main data, click the + for extra data
+								table.row.add( { "title": checklistLink, 
+									"totalNaF": score.totalNotAFinding, "totalNA": score.totalNotApplicable, "totalOpen": score.totalOpen, "totalNR": score.totalNotReviewed,
+									"totalNaFCat1": score.totalCat1NotAFinding, "totalNACat1": score.totalCat1NotApplicable, "totalOpenCat1": score.totalCat1Open, "totalNRCat1": score.totalCat1NotReviewed,
+									"totalNaFCat2": score.totalCat2NotAFinding, "totalNACat2": score.totalCat2NotApplicable, "totalOpenCat2": score.totalCat2Open, "totalNRCat2": score.totalCat2NotReviewed,
+									"totalNaFCat3": score.totalCat3NotAFinding, "totalNACat3": score.totalCat3NotApplicable, "totalOpenCat3": intOpenCat2 = score.totalCat3Open, "totalNRCat3": score.totalCat3NotReviewed
+								}).draw();
+							}
+							else {
+								table.row.add( { "title": checklistLink, 
+									"totalNaF": 0, "totalNA": 0, "totalOpen": 0, "totalNR": 0,
+									"totalNaFCat1": 0, "totalNACat1": 0, "totalOpenCat1": 0, "totalNRCat1": 0,
+									"totalNaFCat2": 0, "totalNACat2": 0, "totalOpenCat2": 0, "totalNRCat2": 0,
+									"totalNaFCat3": 0, "totalNACat3": 0, "totalOpenCat3": 0, "totalNRCat3": 0
+								}).draw();
+							}
+						},
+					error: function() {
+						table.row.add( { "title": checklistLink, 
+									"totalNaF": 0, "totalNA": 0, "totalOpen": 0, "totalNR": 0,
+									"totalNaFCat1": 0, "totalNACat1": 0, "totalOpenCat1": 0, "totalNRCat1": 0,
+									"totalNaFCat2": 0, "totalNACat2": 0, "totalOpenCat2": 0, "totalNRCat2": 0,
+									"totalNaFCat3": 0, "totalNACat3": 0, "totalOpenCat3": 0, "totalNRCat3": 0
+								}).draw();
+					}});
 				}
-			table += '</tbody></tbody></table>'
-			// with all the data fill in the table and go
-			$("#tblChecklistListing").html(table);
-		}
+				// with all the data fill in the table and go
+			  $.unblockUI();
+			}
 	}
-	else 
+	else {
+		$.unblockUI();
 		throw new Error(response.status)
+	}
 }
+
 // called from template listing, calls the POST to the scoring API to get back a score dynamically
 async function getScoreForTemplateListing(xmlChecklist) {
 	var formData = new FormData();
