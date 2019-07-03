f98361a2faedd888cd8e22efd07dbe46cc728138
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-07-02 20:30:22 -0400

    fixing relative paths

--- js/apis.js
+++ js/apis.js
@@ -5,18 +5,8 @@
 // if the ports in the stack.yml are changed, this has to be modified as well. 
 // anyone editing the YAML can edit this file.
 
-// if this is the stack.yml
-if (window.location.port == 8080) { 
-    var readAPI = 'http://localhost:8084'
-    var scoreAPI = 'http://localhost:8090'
-    var saveAPI = 'http://localhost:8082'
-    var uploadAPI = 'http://localhost:8086'
-    var templateAPI = 'http://localhost:8088'
-    var complianceAPI = 'http://localhost:8092'
-    var controlAPI = 'http://localhost:8094'
-} 
 // if this is the dev-stack.yml
-else {
+if (window.location.port == 9080) { 
     var readAPI = 'http://localhost:9084'
     var scoreAPI = 'http://localhost:9090'
     var saveAPI = 'http://localhost:9082'
@@ -24,4 +14,14 @@ else {
     var templateAPI = 'http://localhost:9088'
     var complianceAPI = 'http://localhost:9092'
     var controlAPI = 'http://localhost:9094'
+} 
+// if this is the stack.yml, web, k8s, or other configuration use the real one
+else { 
+    var readAPI = 'http://localhost:8084'
+    var scoreAPI = 'http://localhost:8090'
+    var saveAPI = 'http://localhost:8082'
+    var uploadAPI = 'http://localhost:8086'
+    var templateAPI = 'http://localhost:8088'
+    var complianceAPI = 'http://localhost:8092'
+    var controlAPI = 'http://localhost:8094'
 }
