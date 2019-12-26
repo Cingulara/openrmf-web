a08e65b098725bb4c986721c0d11e91d95b16b4e
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-12-26 18:07:48 -0500

    updating the copyright notice

--- js/apis.js
+++ js/apis.js
@@ -1,3 +1,6 @@
+// Copyright (c) Cingulara 2019. All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 license. See LICENSE file in the project root for full license information.
+
 // loading all APIs here
 // using a configmap in k8s and openshift to set these dynamically works better
 // will be set in the template or xxxxx-configmap.yml file
--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,5 @@
+// Copyright (c) Cingulara 2019. All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 license. See LICENSE file in the project root for full license information.
 var keycloak = Keycloak({
     url: 'http://' + document.location.hostname+ ':9001/auth',
     realm: 'openrmf',
