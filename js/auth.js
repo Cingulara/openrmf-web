a1bec640a277127ad26f630a0d68fb92dba8693a
DaleBinghamSoteriaSoft <dale.bingham@soteriasoft.com>
2023-03-11 10:12:04 -0500

    Make sure Keycloak over HTTPS works

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,7 @@
 // Copyright (c) Cingulara LLC 2020 and Tutela LLC 2020. All rights reserved.
 // Licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 license. See LICENSE file in the project root for full license information.
 var keycloak = Keycloak({
-    url: document.location.protocol + '//' + document.location.hostname+ ':8080/auth',
+    url: document.location.protocol + '//' + document.location.host + '/auth',
     realm: 'openrmf',
     clientId: 'openrmf'
 });
