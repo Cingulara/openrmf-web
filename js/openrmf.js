3f112544d58fb02222d54378680294eed45532c5
DaleBinghamSoteriaSoft <dale.bingham@soteriasoft.com>
2023-11-04 07:56:43 -0400

    Speeding up Template Listing

--- js/openrmf.js
+++ js/openrmf.js
@@ -267,105 +267,110 @@ async function getSystemACASItemsForDashboard() {
  ************************************/
 async function getTemplates(latest) {
 	$.blockUI({ message: "Updating the template listing...please wait", css: { padding: '15px'} });
-	var url = templateAPI;	
-	let response = await fetch(url, {headers: {
-		'Authorization': 'Bearer ' + keycloak.token
-	}});
-	// parse the result regardless of the one called as the DIV are the same on Dashboard/index and the checklists pages
-  if (response.ok) {
-		var data = await response.json()
-		var table = $('#tblChecklistListing').DataTable(); // the datatable reference to do a row.add() to
-		table.clear().draw();
-		var checklistLink = "";
-		if (data.length == 0) {
-			$.unblockUI();
-			var alertText = 'There are no Checklist templates uploaded. Please go to the Upload page to add your first.';
-			alertText += '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
-			alertText += '<span aria-hidden="true">&times;</span></button>';
-			$("#divMessaging").html(alertText);
-			$("#divMessaging").show();
-		}
-		else {
-			$("#divMessaging").html('');
-			$("#divMessaging").hide();
-			for (const item of data) {
-				checklistLink = '<a href="single-template.html?id=' + item.internalIdString + '" title="Open Checklist Template">';
-				checklistLink += item.fullTitle;
-				checklistLink += '</a><br /><span class="small">last updated on ';
-				if (item.updatedOn) {
-					checklistLink += moment(item.updatedOn).format('MM/DD/YYYY h:mm a');
-				}
-				else {
-					checklistLink += moment(item.created).format('MM/DD/YYYY h:mm a');
-				}
-				checklistLink += "</span>";
 
-				// if not a SYSTEM templateType, then get the score; else just fill the table with the listing
-				if (item.templateType == "SYSTEM") {
-					table.row.add( { "title": checklistLink, 
-						"totalNaF": 0, "totalNA": 0, "totalOpen": 0, "totalNR": 0,
-						"totalNaFCat1": 0, "totalNACat1": 0, "totalOpenCat1": 0, "totalNRCat1": 0,
-						"totalNaFCat2": 0, "totalNACat2": 0, "totalOpenCat2": 0, "totalNRCat2": 0,
-						"totalNaFCat3": 0, "totalNACat3": 0, "totalOpenCat3": 0, "totalNRCat3": 0
-					}).draw();
-				} else {
-					// now get the score
-					var score = null;
-					var formData = new FormData();
-					formData.append("rawChecklist", item.rawChecklist);
-					$.ajax({
-						url : scoreAPI,
-						data : formData,
-						async: false,
-						type : 'POST',
-						processData: false,
-						contentType: false,
-						beforeSend: function(request) {
-							request.setRequestHeader("Authorization", 'Bearer ' + keycloak.token);
-						},
-						success : function(data){
-							score = data;
-							if (score) {
-								// dynamically add to the datatable but only show main data, click the + for extra data
-								table.row.add( { "title": checklistLink, 
-									"totalNaF": score.totalNotAFinding, "totalNA": score.totalNotApplicable, "totalOpen": score.totalOpen, "totalNR": score.totalNotReviewed,
-									"totalNaFCat1": score.totalCat1NotAFinding, "totalNACat1": score.totalCat1NotApplicable, "totalOpenCat1": score.totalCat1Open, "totalNRCat1": score.totalCat1NotReviewed,
-									"totalNaFCat2": score.totalCat2NotAFinding, "totalNACat2": score.totalCat2NotApplicable, "totalOpenCat2": score.totalCat2Open, "totalNRCat2": score.totalCat2NotReviewed,
-									"totalNaFCat3": score.totalCat3NotAFinding, "totalNACat3": score.totalCat3NotApplicable, "totalOpenCat3": intOpenCat2 = score.totalCat3Open, "totalNRCat3": score.totalCat3NotReviewed
-								}).draw();
-							}
-							else {
-								table.row.add( { "title": checklistLink, 
-									"totalNaF": 0, "totalNA": 0, "totalOpen": 0, "totalNR": 0,
-									"totalNaFCat1": 0, "totalNACat1": 0, "totalOpenCat1": 0, "totalNRCat1": 0,
-									"totalNaFCat2": 0, "totalNACat2": 0, "totalOpenCat2": 0, "totalNRCat2": 0,
-									"totalNaFCat3": 0, "totalNACat3": 0, "totalOpenCat3": 0, "totalNRCat3": 0
-								}).draw();
-							}
-							$.unblockUI();
-						},
-					error: function() {
-						table.row.add( { "title": checklistLink, 
-							"totalNaF": 0, "totalNA": 0, "totalOpen": 0, "totalNR": 0,
-							"totalNaFCat1": 0, "totalNACat1": 0, "totalOpenCat1": 0, "totalNRCat1": 0,
-							"totalNaFCat2": 0, "totalNACat2": 0, "totalOpenCat2": 0, "totalNRCat2": 0,
-							"totalNaFCat3": 0, "totalNACat3": 0, "totalOpenCat3": 0, "totalNRCat3": 0
-						}).draw();
-						$.unblockUI();
-					}});
-				}
-			}
-			$.unblockUI();
-		}
-	}
-	else {
-		$.unblockUI();
-		if (response.status == 401)
-			swal("There is an Authentication problem. Please logout and log back in. And have the application administrator verify your API's authentication settings.", "Click OK to continue!", "error");
-		else if (response.status == 401)
-			swal("There is an application problem. Please have the application administrator verify your system is 100% healthy and running correctly.", "Click OK to continue!", "error");
-		throw new Error(response.status)
-	}
+	var table = $('#tblChecklistListing').DataTable();
+	table.clear().draw();
+	table.ajax.url(templateAPI).load(finalizeLoadingTable);
+
+	// var url = templateAPI;	
+	// let response = await fetch(url, {headers: {
+	// 	'Authorization': 'Bearer ' + keycloak.token
+	// }});
+	// parse the result regardless of the one called as the DIV are the same on Dashboard/index and the checklists pages
+//   if (response.ok) {
+// 		var data = await response.json()
+// 		var table = $('#tblChecklistListing').DataTable(); // the datatable reference to do a row.add() to
+// 		table.clear().draw();
+// 		var checklistLink = "";
+// 		if (data.length == 0) {
+// 			$.unblockUI();
+// 			var alertText = 'There are no Checklist templates uploaded. Please go to the Upload page to add your first.';
+// 			alertText += '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
+// 			alertText += '<span aria-hidden="true">&times;</span></button>';
+// 			$("#divMessaging").html(alertText);
+// 			$("#divMessaging").show();
+// 		}
+// 		else {
+// 			$("#divMessaging").html('');
+// 			$("#divMessaging").hide();
+// 			for (const item of data) {
+// 				checklistLink = '<a href="single-template.html?id=' + item.internalIdString + '" title="Open Checklist Template">';
+// 				checklistLink += item.fullTitle;
+// 				checklistLink += '</a><br /><span class="small">last updated on ';
+// 				if (item.updatedOn) {
+// 					checklistLink += moment(item.updatedOn).format('MM/DD/YYYY h:mm a');
+// 				}
+// 				else {
+// 					checklistLink += moment(item.created).format('MM/DD/YYYY h:mm a');
+// 				}
+// 				checklistLink += "</span>";
+
+// 				// if not a SYSTEM templateType, then get the score; else just fill the table with the listing
+// 				if (item.templateType == "SYSTEM") {
+// 					table.row.add( { "title": checklistLink, 
+// 						"totalNaF": 0, "totalNA": 0, "totalOpen": 0, "totalNR": 0,
+// 						"totalNaFCat1": 0, "totalNACat1": 0, "totalOpenCat1": 0, "totalNRCat1": 0,
+// 						"totalNaFCat2": 0, "totalNACat2": 0, "totalOpenCat2": 0, "totalNRCat2": 0,
+// 						"totalNaFCat3": 0, "totalNACat3": 0, "totalOpenCat3": 0, "totalNRCat3": 0
+// 					}).draw();
+// 				} else {
+// 					// now get the score
+// 					var score = null;
+// 					var formData = new FormData();
+// 					formData.append("rawChecklist", item.rawChecklist);
+// 					$.ajax({
+// 						url : scoreAPI,
+// 						data : formData,
+// 						async: false,
+// 						type : 'POST',
+// 						processData: false,
+// 						contentType: false,
+// 						beforeSend: function(request) {
+// 							request.setRequestHeader("Authorization", 'Bearer ' + keycloak.token);
+// 						},
+// 						success : function(data){
+// 							score = data;
+// 							if (score) {
+// 								// dynamically add to the datatable but only show main data, click the + for extra data
+// 								table.row.add( { "title": checklistLink, 
+// 									"totalNaF": score.totalNotAFinding, "totalNA": score.totalNotApplicable, "totalOpen": score.totalOpen, "totalNR": score.totalNotReviewed,
+// 									"totalNaFCat1": score.totalCat1NotAFinding, "totalNACat1": score.totalCat1NotApplicable, "totalOpenCat1": score.totalCat1Open, "totalNRCat1": score.totalCat1NotReviewed,
+// 									"totalNaFCat2": score.totalCat2NotAFinding, "totalNACat2": score.totalCat2NotApplicable, "totalOpenCat2": score.totalCat2Open, "totalNRCat2": score.totalCat2NotReviewed,
+// 									"totalNaFCat3": score.totalCat3NotAFinding, "totalNACat3": score.totalCat3NotApplicable, "totalOpenCat3": intOpenCat2 = score.totalCat3Open, "totalNRCat3": score.totalCat3NotReviewed
+// 								}).draw();
+// 							}
+// 							else {
+// 								table.row.add( { "title": checklistLink, 
+// 									"totalNaF": 0, "totalNA": 0, "totalOpen": 0, "totalNR": 0,
+// 									"totalNaFCat1": 0, "totalNACat1": 0, "totalOpenCat1": 0, "totalNRCat1": 0,
+// 									"totalNaFCat2": 0, "totalNACat2": 0, "totalOpenCat2": 0, "totalNRCat2": 0,
+// 									"totalNaFCat3": 0, "totalNACat3": 0, "totalOpenCat3": 0, "totalNRCat3": 0
+// 								}).draw();
+// 							}
+// 							$.unblockUI();
+// 						},
+// 					error: function() {
+// 						table.row.add( { "title": checklistLink, 
+// 							"totalNaF": 0, "totalNA": 0, "totalOpen": 0, "totalNR": 0,
+// 							"totalNaFCat1": 0, "totalNACat1": 0, "totalOpenCat1": 0, "totalNRCat1": 0,
+// 							"totalNaFCat2": 0, "totalNACat2": 0, "totalOpenCat2": 0, "totalNRCat2": 0,
+// 							"totalNaFCat3": 0, "totalNACat3": 0, "totalOpenCat3": 0, "totalNRCat3": 0
+// 						}).draw();
+// 						$.unblockUI();
+// 					}});
+// 				}
+// 			}
+// 			$.unblockUI();
+// 		}
+// 	}
+// 	else {
+// 		$.unblockUI();
+// 		if (response.status == 401)
+// 			swal("There is an Authentication problem. Please logout and log back in. And have the application administrator verify your API's authentication settings.", "Click OK to continue!", "error");
+// 		else if (response.status == 401)
+// 			swal("There is an application problem. Please have the application administrator verify your system is 100% healthy and running correctly.", "Click OK to continue!", "error");
+// 		throw new Error(response.status)
+// 	}
 }
 // called from template listing, calls the POST to the scoring API to get back a score dynamically
 async function getScoreForTemplateListing(xmlChecklist) {
